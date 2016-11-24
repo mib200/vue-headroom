c8c7c574aaf1b22a6b9e0543b5d0043a39b8716b
Manish Kumar <mib200@gmail.com>
2016-11-24 16:05:54 +0530

    es6 to older JS

--- src/checkActions.js
+++ src/checkActions.js
@@ -1,56 +1,41 @@
-function checkActions (states) {
-  const direction = states.currentScrollY >= states.lastScrollY ? 'down' : 'up'
-  const distanceScrolled = Math.abs(states.currentScrollY - states.lastScrollY)
+'use strict';
 
-	var action;
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+function checkActions(states) {
+  var direction = states.currentScrollY >= states.lastScrollY ? 'down' : 'up';
+  var distanceScrolled = Math.abs(states.currentScrollY - states.lastScrollY);
+
+  var action;
 
   // Begining, at the top and not fixed.
-  if (
-    states.currentScrollY <= states.pinStart &&
-    states.state !== 'unfixed'
-  ) {
-    action = 'unfix'
+  if (states.currentScrollY <= states.pinStart && states.state !== 'unfixed') {
+    action = 'unfix';
 
-  // Still unfixed, scrolling down and passing the header.
-  } else if (
-    states.currentScrollY <= states.elemHeight &&
-    direction === 'down' &&
-    states.state === 'unfixed'
-  ) {
-    action = 'none'
+    // Still unfixed, scrolling down and passing the header.
+  } else if (states.currentScrollY <= states.elemHeight && direction === 'down' && states.state === 'unfixed') {
+    action = 'none';
 
-  // Scrolling down and past the header.
-  // Unpinned the header.
-  } else if (
-    direction === 'down' &&
-    ['pinned', 'unfixed'].indexOf(states.state) >= 0 &&
-    states.currentScrollY > (states.elemHeight + states.pinStart) &&
-    distanceScrolled > states.downTolerance
-  ) {
-    action = 'unpin'
+    // Scrolling down and past the header.
+    // Unpinned the header.
+  } else if (direction === 'down' && ['pinned', 'unfixed'].indexOf(states.state) >= 0 && states.currentScrollY > states.elemHeight + states.pinStart && distanceScrolled > states.downTolerance) {
+    action = 'unpin';
 
-  // Now, it's time to up.
-  // Pin the header.
-  } else if (
-    direction === 'up' &&
-    distanceScrolled > states.upTolerance &&
-    ['pinned', 'unfixed'].indexOf(states.state) < 0
-  ) {
-    action = 'pin'
+    // Now, it's time to up.
+    // Pin the header.
+  } else if (direction === 'up' && distanceScrolled > states.upTolerance && ['pinned', 'unfixed'].indexOf(states.state) < 0) {
+    action = 'pin';
 
-  // Still scrolling up and passing the header.
-  // Pin the header regardless of upTolerance.
-  } else if (
-    direction === 'up' &&
-    states.currentScrollY <= states.elemHeight &&
-    ['pinned', 'unfixed'].indexOf(states.state) < 0
-  ) {
-    action = 'pin'
+    // Still scrolling up and passing the header.
+    // Pin the header regardless of upTolerance.
+  } else if (direction === 'up' && states.currentScrollY <= states.elemHeight && ['pinned', 'unfixed'].indexOf(states.state) < 0) {
+    action = 'pin';
   } else {
-    action = 'none'
+    action = 'none';
   }
 
-  return action
+  return action;
 }
 
-export default checkActions
+exports.default = checkActions;
